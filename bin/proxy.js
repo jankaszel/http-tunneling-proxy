587451093c9cbb58e1f7ca5223fbc0c62164c5af
jk <fallafeljan@users.noreply.github.com>
2017-01-21 14:39:02 +0100

    use own `argv` parser

--- bin/proxy.js
+++ bin/proxy.js
@@ -1,27 +1,63 @@
 #!/usr/bin/env node
 
 /* eslint-disable no-console */
+const {basename} = require('path');
 const createProxyServer = require('../');
-const minimist = require('minimist');
+const {version} = require('../package.json');
 
-const args = minimist(process.argv.slice(2), {
-  string: 'lang',
-  boolean: 'pager',
-  alias: {
-    p: 'port',
-    h: 'host'
-  },
-  default: { lang: 'en' },
-  '--': true
-});
+let args = {
+  port: 3030,
+  host: '127.0.0.1'
+};
+
+try {
+  const argv = process.argv.slice(2);
+
+  for (let i = 0; i < argv.length; i++) {
+    if (argv[i] === '-p' || argv[i] === '--port') {
+      const j = ++i;
+      const port = parseInt(argv[j]);
+
+      if (argv.length <= j || !Number.isInteger(port)) {
+        throw new Error();
+      } else {
+        args.port = port;
+      }
+    } else if (argv[i] === 'h' || argv[i] === '--host') {
+      const j = ++i;
+      const host = parseInt(argv[j]);
+
+      if (argv.length <= j) {
+        throw new Error();
+      } else {
+        args.host = host;
+      }
+    } else {
+      throw new Error();
+    }
+  }
+}
+catch (err) {
+  const cmd = basename(process.argv[1]);
+  
+  console.log(`Usage: ${cmd} [options]
+
+Options:
+  -p, --p [port]     Port number
+  -h, --host [name]  Host name
+  --help             Display usage
+
+Version ${version}`);
+
+  process.exit(1);
+}
 
 const proxyServer = createProxyServer(req => {
   console.log(`${req.method} ${req.url}`);
 });
 
-const port = args.port || 3030,
-  host = args.host || '127.0.0.1';
-
+console.log(args)
+const {host, port} = args;
 proxyServer.listen(port, host, err => {
   if (err) {
     console.error(`Error starting proxy to ${host}:${port}: ${err.message}`);
