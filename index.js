e7fdae6e8e594490384ebb33e0bfab6627835c0b
jk <fallafeljan@users.noreply.github.com>
2017-01-24 19:34:08 +0100

    remove socket error handler (we're piping)

--- index.js
+++ index.js
@@ -31,11 +31,6 @@ function createProxyServer(callback = () => {}) {
       srvSocket.write(head);
       srvSocket.pipe(cltSocket);
       cltSocket.pipe(srvSocket);
-
-      srvSocket.on('error', err => {
-        // eslint-disable-next-line no-console
-        console.error(`Remote connection error: ${err.message}`);
-      });
     });
   });
 
